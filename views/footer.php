ef5c9cbc00deaeed064211e587b484a076937244
Erandra Jayasundara <erandrajayasundara@live.com>
2020-02-03 08:01:07 +0530

    added templates and css

---
+++ views/footer.php
@@ -0,0 +1,83 @@
+<footer id="colophon" class="site-footer" role="contentinfo">
+			<div id="tertiary" class="sidebar-container" role="complementary">
+				<div class="container_16">
+					
+					<!-- First Widget Area -->
+					<div class="grid_4">
+						<aside id="meta-0" class="widget widget_adress">
+							<h3>Contact us</h3>
+							<address class="vcard">
+								<p class="org vcard"><a class="url fn org" href="#">ICTCS</a>
+								<p>
+						    <p class="adr">
+							<b>Address</b><br>Department of Physical Science,<br>Faculty of Applied Science<br> Vavuniya Campus,Vavuniya</p>
+						    <p class="adr"><b>Phone:</b><span class="tel"> +94(24)222 0179 </span><br />
+						      <b>E-mail:</b><span class="email"> info@ictcs.lk<br>ictcs@vau.jfn.ac.lk</span><br />
+						      
+					        </p>
+							</address>
+						</aside>
+					</div>
+
+					<!-- Second Widget Area -->
+					<div class="grid_4">
+						<aside id="meta-1" class="widget widget_meta">
+							<h3>Latest Events</h3>
+							<ul>
+								
+								<li><a href="https://hackintra.ictcs.lk" title="Hackintra">Hackintra 2019</a></li>
+								<li><a href="https://twistsnturns.ictcs.lk" title="Twists N' torns">Twists N Turns</a></li>
+								
+							</ul>
+						</aside>						
+					</div>
+					
+					<!-- Third Widget Area -->
+					<div class="grid_4">
+						<aside id="meta-2" class="widget widget_meta">
+							<h3>Get involved</h3>
+							<ul>
+								
+								
+								<li><a href="article-list.php" title="Press">Blog</a></li>
+								<li><a href="contact.php" title="Contact Us">Contact Us</a></li>
+							</ul>
+						</aside>
+					</div>
+					
+					<!-- Forth Widget Area -->
+					<div class="grid_4">
+						<aside id="meta-3" class="widget widget_meta">
+							<h3>Latest Tweets</h3>
+							<div class="latest-tweets-body">
+							<a class="twitter-timeline" height="300" data-theme="dark" data-link-color="#2B7BB9" href="https://twitter.com/ictcsvc?ref_src=twsrc%5Etfw">Tweets by ictcsvc</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+							</div>
+							<div class="clear"></div>
+							
+							<div><a href="https://twitter.com/ictcsvc?ref_src=twsrc%5Etfw" class="radius follow">Follow @ictcsvc</a></div>
+						</aside>
+					</div>
+
+					<div class="clear"></div>
+				</div>
+			</div>
+
+
+			<!-- Site Info -->
+			<div class="site-info">
+				<div class="container_16">
+					
+					<!-- CopyRight -->
+					<div class="grid_8">
+						<p class="copy">Copyright ©  2019.  All Rights reserved.  </p>
+					</div>
+
+					<!-- Design By -->
+					<div class="grid_8">
+						<p class="designby">Powered by Sarasavi Api</p>
+					</div>
+
+					<div class="clear"></div>
+				</div>
+			</div><!-- .site-info -->
+		</footer>
